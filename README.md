782675267baaffe77b3eb66490321822dfa11bcd
dfspray <daniel_spray@comcast.net>
2019-05-14 13:00:05 -0700

    Implemented add new person

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Flask Mailroom Application
 
-Running at [http://afternoon-reef-51666.herokuapp.com/donations/](http://afternoon-reef-51666.herokuapp.com/donations/).
+Running at https://immense-eyrie-62264.herokuapp.com/donations/
 
 ## Your Task
 
--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from model import Donor, Donation 
 
 app = Flask(__name__)
-#app.secret_key = b'Fb\xf6U\xce\xcf\x9eq\xfc\xcc\x84\xba\x91B\xf2\xb0\x17\x07\xdc\x99)NV('
-app.secret_key = os.environ.get('SECRET_KEY').encode()
+app.secret_key = b'Fb\xf6U\xce\xcf\x9eq\xfc\xcc\x84\xba\x91B\xf2\xb0\x17\x07\xdc\x99)NV('
+#app.secret_key = os.environ.get('SECRET_KEY').encode()
 
 @app.route('/')
 def home():
@@ -23,19 +23,27 @@ def create():
 
     if request.method == "POST":
         if request.form['name'] not in [donor.name for donor in Donor.select()]:
-            return render_template('create.jinja2')
-        try:
-            donor_obj = Donor.get(Donor.name==(request.form['name']))
-            donation = int(request.form['amount'])
-            new_Donation = Donation(donor=donor_obj, value=donation)
-            new_Donation.save()
-        except ValueError as val:
-            print(val)
-            return render_template('create.jinja2')
-        except OverflowError as over:
-            print(over)
-            return render_template('create.jinja2')
-        return redirect(url_for('all'))
+            try:
+                new_donor = Donor(name=request.form['name'])
+                new_donor.save()
+                Donation(donor=new_donor, value=request.form['amount']).save()
+                return redirect(url_for('all'))
+            except Exception as ex:
+                print("Whoops! Something went wrong!")
+                print(ex)
+                return render_template('create.jinja2')
+        else:
+            try:
+                donor_obj = Donor.get(Donor.name==(request.form['name']))
+                donation = int(request.form['amount'])
+                new_Donation = Donation(donor=donor_obj, value=donation)
+                new_Donation.save()
+                return redirect(url_for('all'))
+            except Exception as ex:
+                print("Whoops! Something went wrong!")
+                print(ex)
+                return render_template('create.jinja2')
+
     else:
         return render_template('create.jinja2')
 
